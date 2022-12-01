b7d2e987c7b7142d977cf04cc53df8d011de5f6b
Jetpuffed <me@brandonchristoffer.com>
2022-12-01 15:49:15 -0700

    Add helper function for reading in the input files

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+pub mod day_01;
+
+use std::fs;
+use std::io;
+use std::path::PathBuf;
+
+const INPUT_ROOT_PATH: &str = "./inputs/";
+
+pub fn get_input(day: i32, puzzle: i32) -> Result<String, io::Error> {
+    let mut path = PathBuf::from(INPUT_ROOT_PATH);
+    let day_dir = format!("day_{:02}/", day);
+    path.push(day_dir);
+
+    match puzzle {
+        1 => path.push("puzzle_1"),
+        2 => path.push("puzzle_2"),
+        _ => unreachable!(),
+    }
+    fs::read_to_string(path)
+}
